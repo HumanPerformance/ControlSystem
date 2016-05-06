cb11fb0039f5319271ee63748b1ca1de84bd0ee0
fluviolobo <fluvio.lobo@gmail.com>
2016-05-06 18:09:17 +0000

    A "python" directory was created within the Software folder in the ControlSystem repository. Within this directoy, I began development with the functions serialCom.py and arduinoTalk.py in order to attempt several serial communication configurations between the Raspberry Pi and the Arduino.

---
+++ Software/Python/arduinoTalk.py
@@ -0,0 +1,15 @@
+
+#
+# arduinoTalk
+#
+# The following python script was designed to demo the communication between the Raspberry Pi and an Arduino module in a master-slave configuration
+#
+# Fluvio L. Lobo Fenoglietto 05/06/2016
+
+
+#
+# Requirements:
+# a) The 'nanpy-firmaware' must have been installed on the Arduino board to be used as a slave
+
+
+print('hola')
