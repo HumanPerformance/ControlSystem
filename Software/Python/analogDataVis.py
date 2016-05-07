6df61f25acfa17fefc503b7ae22036acaf175f4e
fluviolobo <fluvio.lobo@gmail.com>
2016-05-07 17:51:04 -0400

    Modified the original code according to a video found online with a very specific example on arduinos and sensors. The video has been referenced at the end of the script

--- Software/Python/analogDataVis.py
+++ Software/Python/analogDataVis.py
@@ -13,18 +13,13 @@ Fluvio L. Lobo Fenoglietto 05/06/2016
 from nanpy import SerialManager
 from nanpy import ArduinoApi
 import matplotlib.pyplot as plt
-from matplotlib import style
+from drawnow import *
 import numpy
-
-
-style.use('fivethirtyeight')
-fig  = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
 
 
 #
 # Establishing communication with Arduino board
-connection = SerialManager(device='COM3')
+connection = SerialManager(device='/dev/ttyUSB0')
 
 #
 # Creating Arduino object
@@ -35,18 +30,28 @@ a = ArduinoApi(connection=connection)
 a.pinMode(8, a.OUTPUT)
 a.digitalWrite(8, a.HIGH)
 
+
+
+
 counter = []
 i = 0
 analogVals = []
+
+
+#
+# Plotting function
+plt.ion()
+def makeFig():
+   plt.plot(analogVals)
+
+
 flag = 0
 while flag == 0:
    analogVals.append(a.analogRead(0))
    counter.append(i)
    i = i + 1
-   
-   ax1.clear()
-   ax1.plot(counter,analogVals)
-   plt.show()
+   drawnow(makeFig)
+   plt.pause(0.000001)
        
 
 connection.close()
