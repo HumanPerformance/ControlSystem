ebce5208d9b19761a24997c98b0a37c9a3e62b50
Fluvio Lobo <fluvio.lobo@gmail.com>
2016-05-06 22:16:14 -0400

    Began developing live-plotting capabilities for analog data coming from an arduino module. I have also started to place a reference section at the end of every code. This section has the links to the resources used to build the script.

--- Software/Python/analogDataVis.py
+++ Software/Python/analogDataVis.py
@@ -12,7 +12,15 @@ Fluvio L. Lobo Fenoglietto 05/06/2016
 # Import libraries or modules
 from nanpy import SerialManager
 from nanpy import ArduinoApi
+import matplotlib.pyplot as plt
+from matplotlib import style
 import numpy
+
+
+style.use('fivethirtyeight')
+fig  = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
 
 #
 # Establishing communication with Arduino board
@@ -27,5 +35,29 @@ a = ArduinoApi(connection=connection)
 a.pinMode(8, a.OUTPUT)
 a.digitalWrite(8, a.HIGH)
 
+counter = []
+i = 0
+analogVals = []
+flag = 0
+while flag == 0:
+   analogVals.append(a.analogRead(0))
+   counter.append(i)
+   i = i + 1
+   
+   ax1.clear()
+   ax1.plot(counter,analogVals)
+   plt.show()
+       
+
+connection.close()
+
+
+"""
+
+References:
+1 - http://matplotlib.org/
+2 - https://www.youtube.com/watch?v=zH0MGNJbenc
+
+"""
 
 
