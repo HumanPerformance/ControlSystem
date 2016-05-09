8d414c7da53e0690233542bc7dac8e8705f1b51e
fluviolobo <fluvio.lobo@gmail.com>
2016-05-09 14:37:54 +0000

    Created the python counterpart of the analogDataRelay.ino function. The purpose of this function is to retrieve data printed on the serial port. The current function was tested successfully on the python idle shell

---
+++ Software/Python/analogDataRelay.py
@@ -0,0 +1,33 @@
+"""
+analogDataRelay.py
+
+This script has been designed to retrieved data sent by an arduino module through a specific serial port
+
+Fluvio L. Lobo Fenoglietto 05/09/2016
+
+"""
+
+# Importing Modules
+import serial
+import numpy
+#import matplotlib.pyplot as plt
+#from drawnow import *
+
+# Creating Serial Objects
+arduObj = serial.Serial('/dev/ttyUSB0',115200)
+
+# Defining Variables
+IRmeas = []
+
+
+# Operating Loop
+while True:
+    while (arduObj.inWaiting()==0):
+        pass
+    arduDataString = arduObj.readline()
+    print arduDataString
+
+
+
+
+
