28036a6897a9911381207f7d14de1ecb8a59daaf
fluviolobo <fluvio.lobo@gmail.com>
2016-05-09 14:24:33 +0000

    Began an alternative approach at the communication with the arduino modules embedded in our prototypes. Rather than using firmware that would make the Arduino a slave, we can design a costum algorithm for each arduino module and have the control system code just read the data sent through the serial port.

---
+++ Software/Arduino/analogDataRelay/analogDataRelay.ino
@@ -0,0 +1,23 @@
+/*
+ * analogDataRelay.ino
+ *
+ * The following script has been designed to test an alternative approach at communicating with the Arduino modules, which does not involve any firmware that forces the Arduino to act as a slave.
+ *
+ * Fluvio L. Lobo Fenoglietto 05/09/2016
+ *
+ */
+ 
+ // Variable Initialization
+ int analogPin = 0;
+ int analogVal = 0;
+ 
+ void setup() {
+   Serial.begin(115200);
+ } // End of void setup
+ 
+ void loop() {
+   analogVal = analogRead(analogPin);
+   Serial.println(analogVal);
+   delay(250);
+ } // End of void loop
+ 
