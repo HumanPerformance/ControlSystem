71c84221254593426b4d6ad7efd835ab99ef054f
fluviolobo <fluvio.lobo@gmail.com>
2016-05-10 14:15:44 +0000

    Created a function that combines the two-way communication and the analog data relay routines. The combination of these two sub-routines will be the foundation of the interaction betwwen our arduino modules and the raspberry pi control system.

---
+++ Software/Arduino/twowayDataRelay/twowayDataRelay.ino
@@ -0,0 +1,57 @@
+/*
+ * twowayCom.ino
+ *
+ * Two way communication has been designed to establish an interation between the arduino modules and the control raspberry pi
+ * 
+ * Fluvio L. Lobo Fenoglietto 05/09/2016
+ *
+ */
+ 
+ // Variable Definitions
+ String state = "idle";
+ String arduID = "oto";
+ String inString;
+ 
+ void setup() {
+   
+   Serial.begin(115200); // Default baudrate for the communication with the raspberry pi
+   pinMode(8, OUTPUT);
+   
+ } // End of void setup
+ 
+ void loop() {
+   
+   /*
+    * Communication state check loop
+    */
+   while (state.equals("idle")) {
+     
+     Serial.print(arduID);
+     Serial.print("\n");
+     delay(250);
+     
+     if (Serial.available() > 0) {
+       
+       inString = Serial.readString();
+       
+       if (inString.equals("GO")) {
+         
+         state = "active";
+         digitalWrite(8, HIGH);
+         
+       } // End of if statement :: message check
+       
+     } // End of if statement :: serial port check
+     
+   } // End of while loop :: state check
+   
+   // Recording cue
+   digitalWrite(8, LOW);
+   delay(500);
+   digitalWrite(8, HIGH);
+   delay(500);
+   digitalWrite(8, LOW);
+   delay(500);
+   digitalWrite(8, HIGH);
+   
+ } // End of void loop
