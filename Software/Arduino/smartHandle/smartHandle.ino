a6072befe918d3af790d4c85c4975d2151a8de89
fluviolobo <fluvio.lobo@gmail.com>
2016-05-11 19:38:08 +0000

    Started the development of an arduino code that merges the functionality of both the imu and tof sensors that will be embedded into the smart instrument handle.

---
+++ Software/Arduino/smartHandle/smartHandle.ino
@@ -0,0 +1,288 @@
+/*
+ * smartHandle.ino
+ *
+ * The following script has been designed to control the arduino module and sensors embedded into our instrument handles
+ *
+ * Fluvio L. Lobo Fenoglietto 05/11/2016
+ */
+ 
+/*
+ * Importing Libraries and Modules
+ */
+#include <Wire.h>
+#include <SPI.h>
+#include <SparkFunLSM9DS1.h>
+#include <SparkFun_VL6180X.h>
+/* Note that:
+ * - The user may need to download and install this SparkFun library separately. This code will not install the library!
+ * - The name of the library shown here has been modified by the user to match the name of the local library (as saved in /sketchbook/libraries/...)
+ */
+
+/*
+ * Objects and/or Constructors
+ */
+LSM9DS1 imu;
+
+
+/* 
+ * Sensor Configuration Paramaters and/or Variables
+ */
+
+ // IMU
+#define LSM9DS1_M	0x1E // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG	0x6B // Would be 0x6A if SDO_AG is LOW
+#define PRINT_CALCULATED
+//#define PRINT_RAW
+#define PRINT_SPEED 250 // 250 ms between prints
+// Earth's magnetic field varies by location. Add or subtract 
+// a declination to get a more accurate heading. Calculate 
+// your's here:
+// http://www.ngdc.noaa.gov/geomag-web/#declination
+#define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
+
+ // ToF Range Finder
+#define VL6180X_ADDRESS 0x29
+VL6180xIdentification identification;
+VL6180x sensor(VL6180X_ADDRESS);
+ 
+/*
+ * Void Setup
+ * - Serial Port Configuration
+ * - I2C Configuration
+ * - IMU Configuration
+ * - ToF Range Finder Configuration
+ */
+void setup() {
+  
+  Serial.begin(115200); // Start Serial Library
+  Wire.begin(); // Start I2C Library
+  
+  // Initializing IMU
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+  
+  if (!imu.begin()) {
+    Serial.println("Failed to communicate with LSM9DS1.");
+    Serial.println("Double-check wiring.");
+    Serial.println("Default settings in this sketch will " \
+                  "work for an out of the box LSM9DS1 " \
+                  "Breakout, but may need to be modified " \
+                  "if the board jumpers are.");
+    while (1);
+  } // End of if statement - verification of connection with IMU
+  
+  // Initializing ToF Range Finder
+  sensor.getIdentification(&identification); // Retrieve manufacture info from device memory
+  printIdentification(&identification); // Helper function to print all the Module information
+
+  if(sensor.VL6180xInit() != 0) {
+    Serial.println("FAILED TO INITALIZE"); //Initialize device and check for errors
+  } 
+
+  sensor.VL6180xDefautSettings(); //Load default settings to get started.
+  
+} // End of void setup loop
+
+/*
+ * Void Loop
+ * - IMU Data
+ *   - Gyroscope Data
+ *   - Accelerometer Data
+ *   - Magnetometer Data
+ *   - Attitude: 
+ */
+void loop() {
+  printGyro();  // Print "G: gx, gy, gz"
+  printAccel(); // Print "A: ax, ay, az"
+  printMag();   // Print "M: mx, my, mz"
+  
+  // Print the heading and orientation for fun!
+  // Call print attitude. The LSM9DS1's magnetometer x and y
+  // axes are opposite to the accelerometer, so my and mx are
+  // substituted for each other.
+  printAttitude(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
+  Serial.println();
+  
+  delay(PRINT_SPEED);
+  
+}
+
+void printGyro() {
+  // To read from the gyroscope, you must first call the
+  // readGyro() function. When this exits, it'll update the
+  // gx, gy, and gz variables with the most current data.
+  imu.readGyro();
+  
+  // Now we can use the gx, gy, and gz variables as we please.
+  // Either print them as raw ADC values, or calculated in DPS.
+  Serial.print("G: ");
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcGyro helper function to convert a raw ADC value to
+  // DPS. Give the function the value that you want to convert.
+  Serial.print(imu.calcGyro(imu.gx), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gy), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gz), 2);
+  Serial.println(" deg/s");
+#elif defined PRINT_RAW
+  Serial.print(imu.gx);
+  Serial.print(", ");
+  Serial.print(imu.gy);
+  Serial.print(", ");
+  Serial.println(imu.gz);
+#endif
+
+} // End of function - printGyro()
+
+void printAccel() {
+  // To read from the accelerometer, you must first call the
+  // readAccel() function. When this exits, it'll update the
+  // ax, ay, and az variables with the most current data.
+  imu.readAccel();
+  
+  // Now we can use the ax, ay, and az variables as we please.
+  // Either print them as raw ADC values, or calculated in g's.
+  Serial.print("A: ");
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcAccel helper function to convert a raw ADC value to
+  // g's. Give the function the value that you want to convert.
+  Serial.print(imu.calcAccel(imu.ax), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.ay), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.az), 2);
+  Serial.println(" g");
+#elif defined PRINT_RAW 
+  Serial.print(imu.ax);
+  Serial.print(", ");
+  Serial.print(imu.ay);
+  Serial.print(", ");
+  Serial.println(imu.az);
+#endif
+
+} // End of function - printAccel()
+
+void printMag() {
+  // To read from the magnetometer, you must first call the
+  // readMag() function. When this exits, it'll update the
+  // mx, my, and mz variables with the most current data.
+  imu.readMag();
+  
+  // Now we can use the mx, my, and mz variables as we please.
+  // Either print them as raw ADC values, or calculated in Gauss.
+  Serial.print("M: ");
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcMag helper function to convert a raw ADC value to
+  // Gauss. Give the function the value that you want to convert.
+  Serial.print(imu.calcMag(imu.mx), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcMag(imu.my), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcMag(imu.mz), 2);
+  Serial.println(" gauss");
+#elif defined PRINT_RAW
+  Serial.print(imu.mx);
+  Serial.print(", ");
+  Serial.print(imu.my);
+  Serial.print(", ");
+  Serial.println(imu.mz);
+#endif
+
+} // End of function - printMag()
+
+// Calculate pitch, roll, and heading.
+// Pitch/roll calculations take from this app note:
+// http://cache.freescale.com/files/sensors/doc/app_note/AN3461.pdf?fpsp=1
+// Heading calculations taken from this app note:
+// http://www51.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/Magnetic__Literature_Application_notes-documents/AN203_Compass_Heading_Using_Magnetometers.pdf
+void printAttitude(float ax, float ay, float az, float mx, float my, float mz) {
+  float roll = atan2(ay, az);
+  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
+  
+  float heading;
+  if (my == 0)
+    heading = (mx < 0) ? 180.0 : 0;
+  else
+    heading = atan2(mx, my);
+    
+  heading -= DECLINATION * PI / 180;
+  
+  if (heading > PI) heading -= (2 * PI);
+  else if (heading < -PI) heading += (2 * PI);
+  else if (heading < 0) heading += 2 * PI;
+  
+  // Convert everything from radians to degrees:
+  heading *= 180.0 / PI;
+  pitch *= 180.0 / PI;
+  roll  *= 180.0 / PI;
+  
+  Serial.print("Pitch, Roll: ");
+  Serial.print(pitch, 2);
+  Serial.print(", ");
+  Serial.println(roll, 2);
+  Serial.print("Heading: "); Serial.println(heading, 2);
+  
+} // End of function - printAttitude()
+
+
+
+
+void loop() {
+
+  //Get Ambient Light level and report in LUX
+  Serial.print("Ambient Light Level (Lux) = ");
+  
+  //Input GAIN for light levels, 
+  // GAIN_20     // Actual ALS Gain of 20
+  // GAIN_10     // Actual ALS Gain of 10.32
+  // GAIN_5      // Actual ALS Gain of 5.21
+  // GAIN_2_5    // Actual ALS Gain of 2.60
+  // GAIN_1_67   // Actual ALS Gain of 1.72
+  // GAIN_1_25   // Actual ALS Gain of 1.28
+  // GAIN_1      // Actual ALS Gain of 1.01
+  // GAIN_40     // Actual ALS Gain of 40
+  
+  Serial.println( sensor.getAmbientLight(GAIN_1) );
+
+  //Get Distance and report in mm
+  Serial.print("Distance measured (mm) = ");
+  Serial.println( sensor.getDistance() ); 
+
+  delay(500);  
+};
+
+void printIdentification(struct VL6180xIdentification *temp){
+  Serial.print("Model ID = ");
+  Serial.println(temp->idModel);
+
+  Serial.print("Model Rev = ");
+  Serial.print(temp->idModelRevMajor);
+  Serial.print(".");
+  Serial.println(temp->idModelRevMinor);
+
+  Serial.print("Module Rev = ");
+  Serial.print(temp->idModuleRevMajor);
+  Serial.print(".");
+  Serial.println(temp->idModuleRevMinor);  
+
+  Serial.print("Manufacture Date = ");
+  Serial.print((temp->idDate >> 3) & 0x001F);
+  Serial.print("/");
+  Serial.print((temp->idDate >> 8) & 0x000F);
+  Serial.print("/1");
+  Serial.print((temp->idDate >> 12) & 0x000F);
+  Serial.print(" Phase: ");
+  Serial.println(temp->idDate & 0x0007);
+
+  Serial.print("Manufacture Time (s)= ");
+  Serial.println(temp->idTime * 2);
+  Serial.println();
+  Serial.println();
+}
+
+
