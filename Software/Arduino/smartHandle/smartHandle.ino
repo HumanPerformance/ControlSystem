36ae19d1c97bf8bbe19c3155e481700e4cec625b
fluviolobo <fluvio.lobo@gmail.com>
2016-05-12 14:56:53 +0000

    Successfully merged the arduino codes for the IMU and the ToF Range Finder. The code was uploaded and tested for functionality. Moving forth, changes will be made to simplify the code and print all data in a single line.

--- Software/Arduino/smartHandle/smartHandle.ino
+++ Software/Arduino/smartHandle/smartHandle.ino
@@ -32,8 +32,6 @@ LSM9DS1 imu;
 #define LSM9DS1_M	0x1E // Would be 0x1C if SDO_M is LOW
 #define LSM9DS1_AG	0x6B // Would be 0x6A if SDO_AG is LOW
 #define PRINT_CALCULATED
-//#define PRINT_RAW
-#define PRINT_SPEED 250 // 250 ms between prints
 // Earth's magnetic field varies by location. Add or subtract 
 // a declination to get a more accurate heading. Calculate 
 // your's here:
@@ -104,7 +102,7 @@ void loop() {
   printAttitude(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
   Serial.println();
   
-  delay(PRINT_SPEED);
+  delay(250);
   
 }
 
