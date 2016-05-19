de745fe49d3c1580d0a1b540f87984c880d04205
fluviolobo <fluvio.lobo@gmail.com>
2016-05-19 15:53:05 +0000

    Created a function that will write information to a text file located in a specific, given directory.

---
+++ Software/Python/writeConfig2File.py
@@ -0,0 +1,19 @@
+"""
+writeConfig2File.py
+
+This script has been designed to write specific information to a file stored in a given path/location
+
+Fluvio L. Lobo Fenoglietto 05/19/2016
+
+"""
+
+# Variables
+countdownPath = "/home/pi/csec/repos/ControlSystem/Software/Processing/countdown/application.linux-armv6hf/"
+
+
+
+
+
+"""
+References:
+1- http://stackoverflow.com/questions/5214578/python-print-string-to-text-file
