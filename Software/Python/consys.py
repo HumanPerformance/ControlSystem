1d2753ab3a1f35fb46bbc7b1f451cfb0be04e76d
fluviolobo <fluvio.lobo@gmail.com>
2016-05-19 19:42:15 +0000

    Created a python version of the consys or Control System program designed in Processing for the first Instrument Panle prototype. This function is a much more simplistic algotihtm focused on the communication between arduinos and the raspberry pi and the propper handling and formatting of data. This version of consys lacks a GUI or live plotting as these features slowed the data collection and storage steps in the Processing version. The GUI has been replaced by the Processing program called countdown. This code is also connected to consys.py.

---
+++ Software/Python/consys.py
@@ -0,0 +1,16 @@
+"""
+consys.py
+
+Control System --Python
+
+The following program has been designed to control the following processes:
+1 - Read and follow instruction from servers
+2 - Identification of connected Data Acquisition Modules (DAMs) (Arduino-powered devices)
+3 - Triggering of data collection and retrieval by DAMs
+4 - Storage of data collected and retrieved from DAMs
+4 - Execution of additional application, including timers and countdown clocks
+
+Fluvio L. Lobo Fenoglietto 05/19/2016
+"""
+
+# Import Libraries and/or Modules
