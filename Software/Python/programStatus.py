59f2b9e6cc9b14fb919c13bf269794cb5e80c33e
fluviolobo <fluvio.lobo@gmail.com>
2016-05-19 15:24:12 +0000

    Created a python script capable of checking for the status of a running application.

---
+++ Software/Python/programStatus.py
@@ -0,0 +1,18 @@
+"""
+programStatus.py
+
+This script has been designed to check for the execution status of a given application/program
+
+Fluvio L. Lobo Fenoglietto 05/19/2016
+
+"""
+
+# Import Libraries and/or Modules
+import commands
+
+# Operation
+outStatus = commands.getoutput('ps -A')
+print outStatus
+
+if 'countdown' in outStatus:
+    print "IT'S ALIVE!!"
