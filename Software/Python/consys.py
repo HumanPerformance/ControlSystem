eeef6e34e9012790eb0c637ae76fb27b446e603c
fluviolobo <fluvio.lobo@gmail.com>
2016-05-20 16:24:12 -0400

    Began modifying the consys.py script to contain all the features that have been developed in the past few days. By this commit, the only feature that has been completely implemented, within consys.py, has been the reading inputs from terminal.

--- Software/Python/consys.py
+++ Software/Python/consys.py
@@ -4,13 +4,26 @@ consys.py
 Control System --Python
 
 The following program has been designed to control the following processes:
-1 - Read and follow instruction from servers
-2 - Identification of connected Data Acquisition Modules (DAMs) (Arduino-powered devices)
-3 - Triggering of data collection and retrieval by DAMs
-4 - Storage of data collected and retrieved from DAMs
-4 - Execution of additional application, including timers and countdown clocks
-
+1.0 - Read and follow instruction from server
+    1.1 - Instructions are passed as terminal inputs
+        Note: The program currently handles one (1) input - scalable
+2.0 - Load configuration file based on server instructions or input
+    Note: Configuration file currently located locally - can be switched to the server
+    
 Fluvio L. Lobo Fenoglietto 05/19/2016
 """
 
 # Import Libraries and/or Modules
+import sys
+
+# ==============================================
+# Operation
+# ==============================================
+
+# 1.0 - Read and follow instructions from server
+inputArg = sys.argv
+outString = "User Executed " + inputArg[0]
+print outString
+
+# 2.0 - Load configuration file based on server instruction
+
