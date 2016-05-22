42e41a8a4dd3e13c5a828b09e2cd2811585ba99a
fluviolobo <fluvio.lobo@gmail.com>
2016-05-22 16:25:10 -0400

    Created a variables section on the script where paths/directories are pre-defined. This can be done more easily now with the implementation of the expanduser function. This version of consys should be easily executed in any linux system with the Constrol System repo installed within the /home/user folder of the system, under the  /csec/repos directory structure that is.

--- Software/Python/consys.py
+++ Software/Python/consys.py
@@ -24,10 +24,22 @@ The following program has been designed to control the following processes:
 Fluvio L. Lobo Fenoglietto 05/19/2016
 """
 
+# ==============================================
 # Import Libraries and/or Modules
+# ==============================================
 import sys
 import os
+from os.path import expanduser
 from doubleDigitCorrection import doubleDigitCorrection
+
+# ==============================================
+# Variables
+# ==============================================
+homeDir = expanduser("~")
+consysPyDir = homeDir + "/csec/repos/ControlSystem/Software/Python"
+consysPyDataDir = consysPyDir + "/data"
+countdownDir = homeDir + "/csec/repos/ControlSystem/Software/Processing/countdown/build/armvh6f"
+countdownDataDir = countdownDir + "/data"
 
 # ==============================================
 # Operation
@@ -41,10 +53,9 @@ print outString
 
 # 2.0 - Load configuration file based on server instruction
 ## 2.1 - Define path of configuration file based on user input
-scenarioConfigFilePath = "/home/pi/csec/repos/ControlSystem/Software/Python/data/scenarios/"
-# scenarioConfigFilePath = "/home/fluviolobo/csec/repos/ControlSystem/Software/Python/data/scenarios/"
+scenarioConfigFilePath = consysPyDataDir + "/scenarios"
 scenarioNumberString = doubleDigitCorrection(inputArg[1])
-scenarioConfigFileName = "sc" + scenarioNumberString + ".txt"
+scenarioConfigFileName = "/sc" + scenarioNumberString + ".txt"
 # print scenarioConfigFileName
 ## 2.2 - Load configuration file
 scenarioConfigFile = scenarioConfigFilePath + scenarioConfigFileName
@@ -63,9 +74,8 @@ print int(scenarioConfigValues[1])
 
 # 3.0 - Write loaded variables into downstream configuration file for parallel applications
 ## 3.1 - Define path of configuration file
-countdownConfigFilePath = "/home/pi/csec/repos/ControlSystem/Software/Processing/countdown/data/"
-# countdownConfigFilePath = "/home/fluviolobo/csec/repos/ControlSystem/Software/Processing/countdown/data/"
-countdownConfigFileName = "countdownInit.txt"
+countdownConfigFilePath = countdownDataDir
+countdownConfigFileName = "/countdownInit.txt"
 countdownConfigFile = countdownConfigFilePath + countdownConfigFileName
 ## 3.2 - Write configuration file
 with open(countdownConfigFile, 'r+') as countdownConfigFileObj:
@@ -75,8 +85,8 @@ with open(countdownConfigFile, 'r+') as countdownConfigFileObj:
 
 # 4.0 - Execution of downstream parallel applications
 ## 4.1 - Define path to program executeable
-countdownExeFilePath = "/home/pi/csec/repos/ControlSystem/Software/Processing/countdown/build/armvh6f/"
-countdownExeFileName = "countdown"
+countdownExeFilePath = countdownDir
+countdownExeFileName = "/countdown"
 ## 4.2 - Execute application
 terminalCommand = "sh " + countdownExeFilePath + countdownExeFileName
 os.system(terminalCommand)
