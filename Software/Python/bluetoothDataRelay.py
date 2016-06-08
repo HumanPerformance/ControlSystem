508e00de108e7acf0cfd3705c6faf8dcf2b4c693
fluviolobo <fluvio.lobo@gmail.com>
2016-06-08 01:18:45 +0000

    Currently making several modifications to the bluetoothDataRelay.py script in order to automate connectivity based on information allocated within instrumentconfig.txt. Most of the changes leading to the first final prototyper version of the code wont have much detail.

--- Software/Python/bluetoothDataRelay.py
+++ Software/Python/bluetoothDataRelay.py
@@ -6,22 +6,51 @@ Fluvio L. Lobo Fenoglietto 06/07/2016
 
 # Import Modules
 import os
+from os.path import expanduser
 import serial
 
 # Variable Definitions
-arduID = 'oto\n'
-inString = ''
-stateSwitch = ''
+## General Directories
+homeDir = expanduser("~")
+rootDir = "/root"
+if homeDir == rootDir:
+          homeDir = "/home/pi"
+          # This check and correction is needed for raspbian
+consysPyDir = homeDir + "/csec/repos/ControlSystem/Software/Python"
+consysPyDataDir = consysPyDir + "/data"
+## Instruments Directory
+instrumentsConfigFilePath = consysPyDataDir + "/instruments"
+instrumentsConfigFileName = "/instrumentconfig.txt"
+instrumentsConfigFile = instrumentsConfigFilePath + instrumentsConfigFileName
 
-# Creating Serial Object
+# Creating Bluetooth Serial Objects
+## Identifying Objects from Instruments List
+with open(instrumentsConfigFile,'r+') as instrumentsConfigFileObj:
+    lines = instrumentsConfigFileObj.readlines()
+    #print lines
+Nlines = len(lines)
+instrumentNames = []
+instrumentBTAddress = []
+for i in range(0, Nlines-1):
+    if lines[i][0] != "#": # This comparison allows for the code to skip the comments within the configuration file
+        if lines[i][:10] == "Instrument":
+            instrumentNames.append(lines[i].split(";")[1][:-1])
+        elif lines[i][:16] == "BluetoothAddress":
+            instrumentBTAddress.append(lines[i].split(";")[1][:-1])
+print instrumentNames
+print instrumentBTAddress
+## Using Object Information to Create RFCOMM Ports
+Ndevices = len(instrumentNames)
+#for i in range(0,Ndevices):
+    
 ## PORTS
-port = "rfcomm0"
+#port = "rfcomm0"
 ## Device Address
-address = "00:06:66:7D:98:58"
+#address = "00:06:66:7D:98:58"
 ## Talking to the terminal
-cmd = "sudo rfcomm bind /dev/" + port + " " + address
+#cmd = "sudo rfcomm bind /dev/" + port + " " + address
 ## Sending command
-os.system(cmd)
+#os.system(cmd)
 
 #arduObj = serial.Serial('/dev/rfcomm1',115200)
 
