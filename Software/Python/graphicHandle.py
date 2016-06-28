ed39f9a2275bc1adc5985361d3f3c8096f1c42e0
fluviolobo <fluvio.lobo@gmail.com>
2016-06-28 12:11:02 -0400

    Begand developing an algorithm for the visualization of movement (translation and rotation) of the Smart Handle, usign the data capture by the 9DOFs IMU.

---
+++ Software/Python/graphicHandle.py
@@ -0,0 +1,26 @@
+"""
+graphicHandle.py
+
+The following code has been designed to visualize the motion of the Smart Handle using the data from the 9 DOFs IMU and a few python visualization libraries and/or modules
+
+Fluvio L. Lobo Fenoglietto 06/28/2016
+"""
+
+# ===================================
+# Import Libraries and/or Modules
+# ===================================
+
+# import pywin
+# pywin - Python for Windows - Only needed if working on windows
+import serial
+# if not present:
+# > sudo apt-get install python-serial
+import Vpython
+
+
+"""
+References
+1- Installing Python Serial - https://www.raspberrypi.org/forums/viewtopic.php?f=5&t=5938
+
+
+"""
