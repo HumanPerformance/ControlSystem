4e412670311b97a0443248008c554c8ff972430d
fluviolobo <fluvio.lobo@gmail.com>
2016-09-01 16:37:00 -0400

    Began consolidating all the bluetooth communication functions into one protocol module. The module will be used to control all bluetooth devices connected to the control system.

---
+++ Software/Python/bluetoothProtocol.py
@@ -0,0 +1,19 @@
+"""
+bluetoothProtocol.py
+
+The following module has been created to manage the bluetooth interface between the control system and the connected devices
+
+Michael Xynidis
+Fluvio L Lobo Fenoglietto
+09/01/2016
+
+
+List of functions ::
+
+X - Look for bluetooth device
+X - Pair bluetooth device
+X - Add paired device to the instrument list
+X - Connect to paired device
+
+"""
+
