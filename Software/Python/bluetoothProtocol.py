53487dcc96cfd09eaa8b63c83e3f4801370cef41
fluviolobo <fluvio.lobo@gmail.com>
2016-09-08 15:07:15 -0400

    Began merging bluetooth communication functions previously developed in external scripts into a single protocol script.

--- Software/Python/bluetoothProtocol.py
+++ Software/Python/bluetoothProtocol.py
@@ -17,3 +17,36 @@ X - Connect to paired device
 
 """
 
+# Import Libraries and/or Modules
+import os
+import serial
+from timeStamp import *
+
+# Create RFComm Ports
+#   This function creates radio-frquency (bluetooth) communication ports for specific devices, using their corresponding address
+#   Input   ::  {array/list} "deviceName", "deviceBTAddress"
+#   Output  ::  {array/list} "btObjects"
+def createRFPort(deviceName, deviceBTAddress):
+    Ndevices = len(deviceName) # Determines the number of devices listed
+    RFObject = [] # Create RF object variable/list (in case of multiple devices)
+    for i in range(0,Ndevices):
+        
+        print fullStamp() + " Releasing RFCOMM" + str(i) # Pre-release of RFCOMM port
+        portRelease("rfcomm",i)
+        # Open RFCOMM port for device
+        print fullStamp() + " Connecting device to RFCOMM" + str(i)
+        os.system("sudo rfcomm bind /dev/rfcomm" + str(i) + " " + instrumentBTAddress[i])
+        # Create Arduino RF-Serial Object
+        rfcommPort = "/dev/rfcomm" + str(i)
+        RFObj = serial.Serial(rfcommPort,115200) # Need error handle for the case in which the device is not available
+        arduRFObj.append(RFObj)
+        # Trigger data collection on instruments
+        triggerRFInstrument(arduRFObj[i], instrumentNames[i])
+
+    return arduRFObj
+
+# Connect to paired device
+#   Connects to the bluetooth devices specified by the scenario configuration file
+#   Input   ::  {array/list} "deviceName", "deviceBTAddress"
+#   Output  ::  {array/list} "btObjects"
+
