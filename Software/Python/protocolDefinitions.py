d26180763c84fa8fec27da2f4c7307c22cfc671e
fluviolobo <fluviolobo@gmail.com>
2016-11-02 15:29:02 +0000

    Updated protocol definitions to match the must current communication protocol across our smart devices.

--- Software/Python/protocolDefinitions.py
+++ Software/Python/protocolDefinitions.py
@@ -7,19 +7,35 @@ Michael Xynidis
 Fluvio L Lobo Fenoglietto
 09/26/2016
 """
-# Definition                    Name                        Value           Class
-# ----------                    ----                        -----           -----
-CHK = chr(0x01)         #       System Check                0x01            ORG     
-ENQ = chr(0x05)         #       Enquiry                     0x05            STD
-EOT = chr(0x04)         #       End of Transmission         0x04            STD
-ACK = chr(0x06)         #       Positive Acknowledgement    0x06            STD
-NAK = chr(0x15)         #       Negative Acknowledgement    0x15            STD
-CAN = chr(0x18)         #       Cancel Current Command      0x18            STD
-ESC = chr(0x1B)         #       Escape                      0x1B            STD
-DC1_STRTREC = chr(0x11) #       Start Recording             0x11            ORG
-DC2_STPREC = chr(0x12)  #       Stop Recording              0x12            ORG
-DC3_STRTPLY = chr(0x13) #       Start Playback              0x13            ORG
-DC4_STPPLY = chr(0x14)  #       Stop Playback               0x14            ORG
+# Definition                            Name                                                Value           Class
+# ----------                            ----                                                -----           -----
+ENQ = chr(0x05)                 #       Enquiry                                             0x05            STD
+EOT = chr(0x04)                 #       End of Transmission                                 0x04            STD
+ACK = chr(0x06)                 #       Positive Acknowledgement                            0x06            STD
+NAK = chr(0x15)                 #       Negative Acknowledgement                            0x15            STD
+CAN = chr(0x18)                 #       Cancel Current Command                              0x18            STD
+
+# Device Control Commands
+#   We have extended the four (4) standard "device control" commands by means of a two-byte communication protocol
+
+DC1 = chr(0x11)                 #       Device Control 1: Diagnostic Functions              0x11            STD
+DC1_SDCHECK = chr(0x00)         #           SD Card Check                                   0x00            ORG
+#                                                                                           0xFF            ORG
+
+DC2 = chr(0x12)                 #       Device Control 2: Operational Functions             0x12            STD
+DC2_SENDWAV = chr(0x00)         #           Send .WAV File                                  0x00            ORG
+DC2_DELVOLATILE = chr(0x01)     #           Delete Volatile Files                           0x01            ORG
+#                                                                                           0xFF            ORG
+
+DC3 = chr(0x13)                 #       Device Control 3: Device-Specific Functions         0x13            STD
+DC3_STARTREC = chr(0x00)        #           Start Recording                                 0x00            ORG
+DC3_STOPREC = chr(0x01)         #           Stop Recording                                  0x01            ORG
+DC3_STARTPLAY = chr(0x02)       #           Start Playback                                  0x02            ORG
+DC3_STOPPLAY = chr(0x03)        #           Stop Playback                                   0x03            ORG
+#                                                                                           0xFF            ORG
+
+DC4 = chr(0x14)                 #       Device Control 4: Simulation Functions              0x14            STD
+#                                                                                           0xFF            ORG
 
 # Legend
 # STD - Standard terminology / Standard reference for command
