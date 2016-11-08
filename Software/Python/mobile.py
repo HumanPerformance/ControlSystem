52de0561b7ab5909a621327f72cf7feaae50d3af
fluviolobo <fluvio.lobo@gmail.com>
2016-11-07 21:54:58 -0500

    Began the development of a python-based gui on the master branch. The mobile branch should be removed as using processing does not seem feasible any longer.

---
+++ Software/Python/mobile.py
@@ -0,0 +1,24 @@
+"""
+mobile.py
+"""
+
+
+from Tkinter import *                                   # GUI design libraries
+import ttk                                              # ...
+
+from mobileFunctions import findStethoscope
+
+gui = Tk()                                              # Initialization of the window under object name "root"
+gui.title("mobile.py")                                  # Title of the window
+gui.geometry('450x450+200+200')                         # Window dimensions in pixels + the distance from the top-left corner of your screen
+
+# Information Label
+infoLabel = Label(text="SMART STETHOSCOPE")
+infoLabel.place(x=10,y=10)
+
+# Action Buttons
+searchDevicesButton = Button(text="Find Stethoscope", command=findStethoscope)
+searchDevicesButton.place(x=10,y=50)
+
+gui.mainloop()
+
---
+++ Software/Python/mobileFunctions.py
@@ -0,0 +1,21 @@
+"""
+mobileFunctions.py
+
+This module has been created to contain all of the functions to be called by the mobile script
+
+Fluvio L Lobo Fenoglietto
+07/11/2016
+"""
+
+# Import Modules
+
+
+# Functions
+
+# Find Stethoscope
+#   Function designed to find a smart stethoscope in range
+#   This function coordinates several functions from various imported protocols
+#   Input   ::  None
+#   Output  ::  {array/list}
+def findStethoscope():
+    print "Stethoscope Found"
