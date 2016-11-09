bce412f9bfbdf0a4e2f4df02c740d77de5deab9b
Fluvio Lobo <fluvio.lobo@gmail.com>
2016-11-09 10:05:35 -0500

    In order to trigger functions and pass variables between them, functions must be defined within the same module as the gui. I am not entirely sure if there is an alternative, but the current method works. I have deleted the companion functions module as a result.

--- Software/Python/mobileWin.py
+++ Software/Python/mobileWin.py
@@ -5,8 +5,33 @@ mobile.py
 # Import Libraries and/or Modules
 from Tkinter import *                                   # GUI design libraries
 import ttk                                              # ...
-from mobileFunctionsWin import findStethoscope
+from timeStamp import fullStamp
+import protocolDefinitions
+from bluetoothProtocolWin import findDevices
+from bluetoothProtocolWin import findSmartDevice
+from bluetoothProtocolWin import nextAvailablePort
+from bluetoothProtocolWin import createPort
 
+
+# Functions
+
+# Find Stethoscope
+#   Function designed to find a smart stethoscope in range
+#   This function coordinates several functions from various imported protocols
+#   Input   ::  None
+#   Output  ::  {array/list}
+def findStethoscope():
+    print fullStamp() + " findStethoscope()"
+    global smartDeviceName
+    availableDeviceNames, availableDeviceBTAddresses = findDevices()
+    smartDeviceName, smartDeviceBTAddress = findSmartDevice("RNBT-76E6",availableDeviceNames, availableDeviceBTAddresses)
+    portName = nextAvailablePort()
+    rfObject = createPort(portName,smartDeviceName,smartDeviceBTAddress)
+
+def printDeviceInfo(smartDeviceName):
+    print smartDeviceName
+
+# Graphical User Interface (GUI)
 
 gui = Tk()                                              # Initialization of the window under object name "root"
 gui.title("mobile.py")                                  # Title of the window
@@ -19,6 +44,10 @@ infoLabel.place(x=10,y=10)
 # Action Buttons
 searchDevicesButton = Button(text="Find Stethoscope", command=findStethoscope)
 searchDevicesButton.place(x=10,y=50)
+
+# Action Buttons
+searchDevicesButton = Button(text="Print Device Name", command= lambda: printDeviceInfo(smartDeviceName))
+searchDevicesButton.place(x=10,y=200)
 
 
 gui.mainloop()
