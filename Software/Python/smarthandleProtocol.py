c53b3c42d427359f79d628b7069f67d723f89918
fluviolobo <fluviolobo@gmail.com>
2016-11-21 16:02:10 +0000

    Added standard communication functions and the first diagnostic function, device identification. These will be use to test the communication between the device and the control system.

--- Software/Python/smarthandleProtocol.py
+++ Software/Python/smarthandleProtocol.py
@@ -17,4 +17,54 @@ from configurationProtocol import *
 from bluetoothProtocol import *
 import protocolDefinitions as definitions
 
+# Standard Functions
+#       These functions are standard from the ASCii communication table
 
+# State Enquiry
+#       This function requests the status of then stethoscope
+#       Input   ::      rfObject                {object}        serial object
+#                       timeout                 {int}           maximum wait time for serial communication
+#                       iterCheck               {int}           maximum number of iterations for serial communication
+#       Output  ::      terminal messages       {string}        terminal messages for logging
+def statusEnquiry(rfObject, timeout, iterCheck):
+        print fullStamp() + " statusEnquiry()"                                                                  # Print function name
+        outByte = definitions.ENQ                                                                               # Send ENQ / Status Enquiry command - see protocolDefinitions.py
+        inByte = sendUntilRead(rfObject, outByte, timeout, iterCheck)                                           # Execute sendUntilRead() from bluetoothProtocol.py
+        if inByte == definitions.ACK:                                                                           # Check for ACK / NAK response found through sendUntilRead()
+                print fullStamp() + " ACK Device READY"                                                         # ACK, in this case, translates to DEVICE READY
+        elif inByte == definitions.NAK:                                                                         # Check for ACK / NAK response found through sendUntilRead()
+                print fullStamp() + " NAK Device NOT READY"                                                     # NAK, in this case, translates to DEVICE NOT READY
+
+# System Check
+#       This function commands the connected stethoscope to perform a "systems check", which may consist on a routine verification of remote features
+#       Input   ::      rfObject                {object}        serial object
+#                       timeout                 {int}           maximum wait time for serial communication
+#                       iterCheck               {int}           maximum number of iterations for serial communication
+#       Output  ::      terminal messages       {string}        terminal messages for logging
+def systemCheck(rfObject, timeout, iterCheck):
+        print fullStamp() + " systemCheck()"                                                                    # Print function name
+        outByte = definitions.CHK                                                                               # Send CHK / System Check command - see protocolDefinitions.py
+        inByte = sendUntilRead(rfObject, outByte, timeout, iterCheck)                                           # Execute sendUntilRead() from bluetoothProtocol.py
+        if inByte == definitions.ACK:                                                                           # Check for ACK / NAK response found through sendUntilRead()
+                print fullStamp() + " ACK SD Card Check Passed"                                                 # If the SD card check is successful, the remote device sends a ACK
+                print fullStamp() + " ACK Device Ready"                                                         # ACK, in this case, translates to DEVICE READY
+        elif inByte == definitions.NAK:                                                                         # Check for ACK / NAK response found through sendUntilRead()
+                print fullStamp() + " NAK SD Card Check Failed"                                                 # If the SD card check fails, the remote device sends a NAK
+                print fullStamp() + " NAK Device NOT Ready"  
+
+# Diagnostic Functions
+#       These functions deal with the status of the hardware
+
+# Device Identification
+#       This function requests the identification of the connected device
+#       Input   ::      {object}                rfObject                serial object
+#       Output  ::      {string}                terminal message        terminal message
+def deviceID(rfObject):
+        outBytes = [definitions.DC1, definitions.DC1_DEVICEID]                                                  # Store the sequence of bytes associated with the operation, function, feature
+        inBytes = []
+        for i in range(0,len(outBytes)):                                                                        # For loop for the sequential delivery of bytes using the length of the sequence for the range
+                rfObject.write(outBytes[i])
+                if i == (len(outBytes) - 1):                                                                    # On the last byte, the program reads the response
+                        for i in range(0,3):
+                                inBytes.append(rfObject.read(size=1))
+        print inBytes
