a65c62103e017dd0dff3412e109d89af529fc43c
fluviolobo <fluviolobo@gmail.com>
2016-11-21 15:59:08 +0000

    Started the smart handle branch of the control system repo with the python protocol script for the smart handle.

---
+++ Software/Python/smarthandleProtocol.py
@@ -0,0 +1,20 @@
+"""
+smarthandleProtocol.py
+
+The following module has been created to manage the interface between the smart handle module and the control system
+
+Fluvio L Lobo Fenoglietto
+11/21/2016
+
+"""
+
+# Import Libraries and/or Modules
+import os
+import sys
+import serial
+from timeStamp import *
+from configurationProtocol import *
+from bluetoothProtocol import *
+import protocolDefinitions as definitions
+
+
