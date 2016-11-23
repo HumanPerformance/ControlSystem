06b2f6ddf342f05a6c5e73433c2aae4f23e19531
Fluvio Lobo <fluvio.lobo@gmail.com>
2016-11-23 14:54:02 -0500

    After consolidating all branches together, a new file convention has been agreed upon.

---
+++ Software/Python/thermometerTest.py
@@ -0,0 +1,58 @@
+
+# Import
+import sys
+import os
+from os.path import expanduser
+import serial
+from configurationProtocol import *
+from bluetoothProtocol import *
+from stethoscopeProtocol import *
+
+# Variables
+# ----------------------------------------------
+# B) Path/Directory Variables
+# ----------------------------------------------
+homeDir = expanduser("~")
+rootDir = "/root"
+if homeDir == rootDir:
+          homeDir = "/home/pi"
+          # This check and correction is needed for raspbian
+# .../Python
+consysPyDir = homeDir + "/csec/repos/ControlSystem/Software/Python"
+# .../Python/data
+consysPyDataDir = consysPyDir + "/data"
+# .../Python/data/scenarios
+scenarioConfigFilePath = consysPyDataDir + "/scenarios"
+
+# Scenario File Name
+scenarioFileName = "sc001.xml"
+
+configFile = scenarioConfigFilePath + "/" + scenarioFileName
+tree, root = readConfigFile(configFile)
+
+# =========
+# OPERATION
+# =========
+
+#deviceName, deviceBTAddress = pullInstruments(tree, root)   # pull instrument information from configuration file
+deviceName = ["hola"]
+deviceBTAddress = ["00:06:66:86:76:E6"]
+rfObject = createPort(deviceName, deviceBTAddress)          # create rfObjects/ports
+
+#sendUntilRead(rfObject[0],0x05)
+
+deviceID(rfObject)
+#statusEnquiry(rfObject,5,5)
+#sdCardCheck(rfObject)
+#systemCheck(rfObject[0],5,5)
+#startRecording(rfObject)
+#stopRecording(rfObject)
+#escapeOp(rfObject[0],5,5)
+
+#timedRead(rfObject[0],5)
+
+#for i in range(0,50):
+#    rfObject[0].write(chr(0x05))
+#    print str(i)
+
+portRelease('rfcomm', 0)                                    # Release port to avoid permanent connection
