3dd11ab6e0ac43730f124c865ed0ae4ab5019309
root <fluviolobo@gmail.com>
2016-12-23 18:04:53 +0000

    Modified the stethoscope test script to go through a simple recording scenario.

--- Software/Python/stethoscopeTest.py
+++ Software/Python/stethoscopeTest.py
@@ -2,50 +2,47 @@
 # Import
 import sys
 import os
-from os.path import expanduser
 import serial
-from configurationProtocol import *
-from bluetoothProtocol import *
-from stethoscopeProtocol import *
-
-# Variables
-# ----------------------------------------------
-# B) Path/Directory Variables
-# ----------------------------------------------
-homeDir = expanduser("~")
-rootDir = "/root"
-if homeDir == rootDir:
-          homeDir = "/home/pi"
-          # This check and correction is needed for raspbian
-# .../Python
-consysPyDir = homeDir + "/csec/repos/ControlSystem/Software/Python"
-# .../Python/data
-consysPyDataDir = consysPyDir + "/data"
-# .../Python/data/scenarios
-scenarioConfigFilePath = consysPyDataDir + "/scenarios"
-
-# Scenario File Name
-scenarioFileName = "sc001.xml"
-
-configFile = scenarioConfigFilePath + "/" + scenarioFileName
-tree, root = readConfigFile(configFile)
+from   os.path               import expanduser
+from   configurationProtocol import *
+from   bluetoothProtocol     import *
+from   stethoscopeProtocol   import *
 
 # =========
 # OPERATION
 # =========
 
-deviceName = "stet"
-deviceBTAddress = "00:06:66:86:76:E6"                              # stethoscope prototype 
-#deviceBTAddress = "00:06:66:7D:80:D0"                               # POV stethoscope
-rfObject = createPort(deviceName, deviceBTAddress, 115200, 5)       # create rfObjects/ports
+deviceName = "SS"
+deviceBTAddress = "00:06:66:86:60:3D"
+rfObject = createPort(deviceName, deviceBTAddress, 115200, 5, 5)
 
-#statusEnquiry(rfObject)
-startTrackingMicStream(rfObject)
+time.sleep(1)
+if rfObject.isOpen() == False:
+    rfObject.open()
+sdCardCheck(rfObject)
+rfObject.close()
 
-#while(1):
-#    outString = rfObject.read(size=1)
-#    print outString
+time.sleep(2)
+if rfObject.isOpen() == False:
+    rfObject.open()
+startRecording(rfObject)
+rfObject.close()
 
-#stopTrackingMicStream(rfObject)
 
-portRelease('rfcomm', 0)                                    # Release port to avoid permanent connection
+time.sleep(30)
+
+time.sleep(2)
+if rfObject.isOpen() == False:
+    rfObject.open()
+stopRecording(rfObject)
+rfObject.close()
+
+"""
+time.sleep(1)
+if rfObject.isOpen() == False:
+    rfObject.open()
+sdCardCheck(rfObject)
+rfObject.close()
+"""
+
+portRelease('rfcomm', 0)
