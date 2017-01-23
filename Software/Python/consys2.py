fc3d8f60a87ecf7d368b3816a8773b4c3898439e
moe93 <m.odeh93@gmail.com>
2017-01-23 13:43:11 -0500

    commit

--- Software/Python/consys2.py
+++ Software/Python/consys2.py
@@ -22,12 +22,17 @@ import  sys
 import  os
 import  serial
 import  time
+import  parallelpy
 from    os.path                import expanduser
 
 # PD3D Modules
 from    timeStamp              import fullStamp
-from    sequentialPrompt       import *
-
+from    sequentialPrompt       import timerApp
+from    configurationProtocol  import *
+from    bluetoothProtocol      import *
+from    usbProtocol            import *
+from    smarthandleProtocol    import *
+from    smartHolderProtocol    import *
 
 # ==============================================
 # Variables
@@ -97,6 +102,130 @@ deviceIndex, deviceTypes, deviceNames, deviceAddresses = instrumentCrossReferenc
 # Operation
 # ==============================================
 
+# ----------------------------------------------
+# Pre-Simulation / Configuration Loop
+#   The following while-loop will preceed the simulatio loop
+#   This loop works as a configuration step
+# ----------------------------------------------
+def startSim():
+    print fullStamp() + " Start Configuration"
+
+    # Creating Serial Port for Devices
+    time.sleep(1)
+    print fullStamp() + " Creating port for SH0"
+    #sh0 = createPortS(deviceTypes[0],0,deviceAddresses[0],115200,5)
+
+    time.sleep(1)
+    print fullStamp() + " Creating port for SH1"
+    #sh1 = createPortS(deviceTypes[1],1,deviceAddresses[1],115200,5)
+
+
+    time.sleep(1)
+    print fullStamp() + " Connecting Smart Holder"
+    #hld = createUSBPort(deviceTypes[2],1,115200,5)
+
+    # Triggering Smart Handle Devices
+    time.sleep(1)
+    print fullStamp() + " Triggering SH0"
+    #triggerDevice2(sh0,"SH")
+
+    time.sleep(1)
+    print fullStamp() + " Triggering SH1"
+    #triggerDevice2(sh1,"SH")
+
+    time.sleep(1)
+    print fullStamp() + " Triggering Smart Holder"
+    #triggerDevice(hld,deviceTypes[2])
+
+    # Openning Ports
+    time.sleep(1)
+    print fullStamp() + " Openning Serial Port to SH0"
+    #if sh0.isOpen() == False:
+        #sh0.open()
+
+    time.sleep(1)
+    print fullStamp() + " Openning Serial Port to SH1"
+    #if sh1.isOpen() == False:
+        #sh1.open()
+
+    time.sleep(1)
+    print fullStamp() + " Opening Serial Port to Smart Holder"
+    #if hld.isOpen() == False:
+        #hld.open()
+
+# ----------------------------------------------
+# Simulation / Configuration Loop
+#   In this loop, connected devices will be accessed for data collection
+# ----------------------------------------------
+
+    time.sleep(5) # Adding this wait actually improved the number of values read?!?!? - use the first timer
+    simStartTime = time.time()
+    simCurrentTime = 0
+    simStopTime = timers[0] # currently just using the initial timer
+    # simLoopCounter = 0
+    dataStream = []
+    print fullStamp() + " Starting Simulation Loop, time = %.03f seconds" %simStopTime
+
+    try:
+        while simCurrentTime < simStopTime:
+            '''
+            # Handles
+            dataStream.append(["%.02f" %simCurrentTime,
+                               sh0.readline()[:-1],
+                               sh1.readline()[:-1],
+                               hld.readline()[:-1]])
+            
+            simCurrentTime = time.time() - simStartTime
+            print fullStamp() + " Current Simulation Time = %.03f" %simCurrentTime
+            # simLoopCounter = simLoopCounter + 1;
+
+            # End of Simulation Loop
+            '''
+    except Exception as instance:
+        print fullStamp() + " Exception or Error Caught"
+        print fullStamp() + " Error Type " + str(type(instance))
+        print fullStamp() + " Error Arguments " + str(instance.args)
+        print fullStamp() + " Closing Open Ports"
+        
+        time.sleep(1)
+        #if sh0.isOpen() == True:
+            #sh0.close()
+
+        time.sleep(1)
+        #if sh1.isOpen() == True:
+            #sh1.close()
+
+        time.sleep(1)
+        #if hld.isOpen == True:
+            #hld.close()
+                        
+# print dataStream
+'''
+    time.sleep(0.25)
+    if sh0.isOpen() == True:
+        sh0.close()
+
+    time.sleep(0.25)
+    if sh1.isOpen() == True:
+        sh1.close()
+
+    time.sleep(0.25)
+    if hld.isOpen == True:
+        hld.close()
+                              
+    time.sleep(0.25)
+    stopDevice2(sh0,deviceTypes[0])
+
+    time.sleep(0.25)
+    stopDevice2(sh1,deviceTypes[1])
+
+    time.sleep(0.25)                                          
+    stopDevice(hld,deviceTypes[2])
+'''
+# ----------------------------------------------
+# Data Storage
+# ----------------------------------------------
+
 ###
 # timerApp(timer1, timer2, timer3, direction)
 # timer = time in SECONDS
@@ -104,12 +233,7 @@ deviceIndex, deviceTypes, deviceNames, deviceAddresses = instrumentCrossReferenc
 # direction = "down" to start from timer's upper bound downwards
 ###
 
-timerApp(30, 30, 15, "down")
-
-
-# ----------------------------------------------
-# Data Storage
-# ----------------------------------------------
+parallelpy.run(timerApp(30, timers[0], 15, "down"), startSim())
 
 # Print data on device-specific text files
 Ndevices = len(deviceNames)
