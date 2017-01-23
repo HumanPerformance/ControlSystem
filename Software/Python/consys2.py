dcd62063f7b7f49c30acf4d2afd88398091bccc7
moe93 <m.odeh93@gmail.com>
2017-01-23 14:01:19 -0500

    commit

--- Software/Python/consys2.py
+++ Software/Python/consys2.py
@@ -22,17 +22,12 @@ import  sys
 import  os
 import  serial
 import  time
-import  parallelpy
 from    os.path                import expanduser
 
 # PD3D Modules
 from    timeStamp              import fullStamp
-from    sequentialPrompt       import timerApp
-from    configurationProtocol  import *
-from    bluetoothProtocol      import *
-from    usbProtocol            import *
-from    smarthandleProtocol    import *
-from    smartHolderProtocol    import *
+from    sequentialPrompt       import *
+
 
 # ==============================================
 # Variables
@@ -102,138 +97,19 @@ deviceIndex, deviceTypes, deviceNames, deviceAddresses = instrumentCrossReferenc
 # Operation
 # ==============================================
 
-# ----------------------------------------------
-# Pre-Simulation / Configuration Loop
-#   The following while-loop will preceed the simulatio loop
-#   This loop works as a configuration step
-# ----------------------------------------------
-def startSim():
-    print fullStamp() + " Start Configuration"
-
-    # Creating Serial Port for Devices
-    time.sleep(1)
-    print fullStamp() + " Creating port for SH0"
-    #sh0 = createPortS(deviceTypes[0],0,deviceAddresses[0],115200,5)
-
-    time.sleep(1)
-    print fullStamp() + " Creating port for SH1"
-    #sh1 = createPortS(deviceTypes[1],1,deviceAddresses[1],115200,5)
-
-
-    time.sleep(1)
-    print fullStamp() + " Connecting Smart Holder"
-    #hld = createUSBPort(deviceTypes[2],1,115200,5)
-
-    # Triggering Smart Handle Devices
-    time.sleep(1)
-    print fullStamp() + " Triggering SH0"
-    #triggerDevice2(sh0,"SH")
-
-    time.sleep(1)
-    print fullStamp() + " Triggering SH1"
-    #triggerDevice2(sh1,"SH")
-
-    time.sleep(1)
-    print fullStamp() + " Triggering Smart Holder"
-    #triggerDevice(hld,deviceTypes[2])
-
-    # Openning Ports
-    time.sleep(1)
-    print fullStamp() + " Openning Serial Port to SH0"
-    #if sh0.isOpen() == False:
-        #sh0.open()
-
-    time.sleep(1)
-    print fullStamp() + " Openning Serial Port to SH1"
-    #if sh1.isOpen() == False:
-        #sh1.open()
-
-    time.sleep(1)
-    print fullStamp() + " Opening Serial Port to Smart Holder"
-    #if hld.isOpen() == False:
-        #hld.open()
-
-# ----------------------------------------------
-# Simulation / Configuration Loop
-#   In this loop, connected devices will be accessed for data collection
-# ----------------------------------------------
-
-    time.sleep(5) # Adding this wait actually improved the number of values read?!?!? - use the first timer
-    simStartTime = time.time()
-    simCurrentTime = 0
-    simStopTime = timers[0] # currently just using the initial timer
-    # simLoopCounter = 0
-    dataStream = []
-    print fullStamp() + " Starting Simulation Loop, time = %.03f seconds" %simStopTime
-
-    try:
-        while simCurrentTime < simStopTime:
-            '''
-            # Handles
-            dataStream.append(["%.02f" %simCurrentTime,
-                               sh0.readline()[:-1],
-                               sh1.readline()[:-1],
-                               hld.readline()[:-1]])
-            
-            simCurrentTime = time.time() - simStartTime
-            print fullStamp() + " Current Simulation Time = %.03f" %simCurrentTime
-            # simLoopCounter = simLoopCounter + 1;
-
-            # End of Simulation Loop
-            '''
-    except Exception as instance:
-        print fullStamp() + " Exception or Error Caught"
-        print fullStamp() + " Error Type " + str(type(instance))
-        print fullStamp() + " Error Arguments " + str(instance.args)
-        print fullStamp() + " Closing Open Ports"
-        
-        time.sleep(1)
-        #if sh0.isOpen() == True:
-            #sh0.close()
-
-        time.sleep(1)
-        #if sh1.isOpen() == True:
-            #sh1.close()
-
-        time.sleep(1)
-        #if hld.isOpen == True:
-            #hld.close()
-                        
-# print dataStream
-'''
-    time.sleep(0.25)
-    if sh0.isOpen() == True:
-        sh0.close()
-
-    time.sleep(0.25)
-    if sh1.isOpen() == True:
-        sh1.close()
-
-    time.sleep(0.25)
-    if hld.isOpen == True:
-        hld.close()
-                              
-    time.sleep(0.25)
-    stopDevice2(sh0,deviceTypes[0])
-
-    time.sleep(0.25)
-    stopDevice2(sh1,deviceTypes[1])
-
-    time.sleep(0.25)                                          
-    stopDevice(hld,deviceTypes[2])
-'''
-# ----------------------------------------------
-# Data Storage
-# ----------------------------------------------
-
 ###
 # timerApp(timer1, timer2, timer3, direction)
 # timer = time in SECONDS
 # direction = "up" to start counting from 0 upwards
 # direction = "down" to start from timer's upper bound downwards
 ###
-commands=['timerApp(30, timers[0], 15, "down")', 'startSim']
-parallelpy.run(timerApp(30, timers[0], 15, "down"), startSim())
+
+timerApp(30, 30, 15, "down")
+
+
+# ----------------------------------------------
+# Data Storage
+# ----------------------------------------------
 
 # Print data on device-specific text files
 Ndevices = len(deviceNames)
--- Software/Python/sequentialPrompt.py
+++ Software/Python/sequentialPrompt.py
@@ -9,15 +9,22 @@ Date: Dec. 20th 2016
 # t is time in seconds
 # direction is up/down for stopwatch/countdown
 '''
-from Tkinter import *
-from timeStamp import *
+from    Tkinter                import *
+from    configurationProtocol  import *
+from    bluetoothProtocol      import *
+from    usbProtocol            import *
+from    smarthandleProtocol    import *
+from    smartHolderProtocol    import *
+from    timeStamp              import fullStamp
 import time
 import threading
 import StopWatchModule
 
 
+
+
 def phaseOne(t1):
-    print fullStamp() + " Phase One"
+    print fullStamp() + " Entering Configuration Loop"
     if mode is 'countDown':
         sw.Reset(t1)
         sw.countDown(t1)
@@ -26,8 +33,57 @@ def phaseOne(t1):
 
     return
 
-def phaseTwo(t2):
-    print fullStamp() + " Phase Two"
+# ----------------------------------------------
+# Simulation / Configuration Loop
+#   In this loop, connected devices will be accessed for data collection
+# ----------------------------------------------
+
+def fetchData(t1,t2):
+    print fullStamp() + " Fetching Data"
+    #time.sleep(5) # Adding this wait actually improved the number of values read?!?!? - use the first timer
+    simStartTime = time.time()
+    simCurrentTime = 0
+    simStopTime = t1+10 # currently just using the initial timer
+    # simLoopCounter = 0
+    dataStream = []
+    print fullStamp() + " Starting Simulation Loop, time = %.03f seconds" %simStopTime
+    
+    try:
+        while simCurrentTime < simStopTime:
+            
+            # Handles
+            '''
+            dataStream.append(["%.02f" %simCurrentTime,
+                               sh0.readline()[:-1],
+                               sh1.readline()[:-1],
+                               hld.readline()[:-1]])
+            '''
+            simCurrentTime = time.time() - simStartTime
+            print fullStamp() + " Current Simulation Time = %.03f" %simCurrentTime
+            # simLoopCounter = simLoopCounter + 1;
+
+            # End of Simulation Loop
+            
+    except Exception as instance:
+        print fullStamp() + " Exception or Error Caught"
+        print fullStamp() + " Error Type " + str(type(instance))
+        print fullStamp() + " Error Arguments " + str(instance.args)
+        print fullStamp() + " Closing Open Ports"
+
+        '''
+        time.sleep(1)
+        if sh0.isOpen() == True:
+            sh0.close()
+
+        time.sleep(1)
+        if sh1.isOpen() == True:
+            sh1.close()
+
+        time.sleep(1)
+        if hld.isOpen == True:
+            hld.close()
+        '''     
+
     if mode is 'countDown':
         sw.Reset(t2)
         sw.countDown(t2)
@@ -36,8 +92,30 @@ def phaseTwo(t2):
 
     return
 
-def phaseThree():
-    print fullStamp() + " Phase Three"
+def closePorts():
+    print fullStamp() + " Terminating"
+    '''
+    time.sleep(0.25)
+    if sh0.isOpen() == True:
+        sh0.close()
+
+    time.sleep(0.25)
+    if sh1.isOpen() == True:
+        sh1.close()
+
+    time.sleep(0.25)
+    if hld.isOpen == True:
+        hld.close()
+                              
+    time.sleep(0.25)
+    stopDevice2(sh0,deviceTypes[0])
+
+    time.sleep(0.25)
+    stopDevice2(sh1,deviceTypes[1])
+
+    time.sleep(0.25)                                          
+    stopDevice(hld,deviceTypes[2])
+    '''    
     if mode is 'countDown':
         pass
     elif mode is 'stopWatch':
@@ -58,7 +136,10 @@ def end_fullscreen(self, event=None):
     root.attributes("-fullscreen", False)
     return "break"
 
-def timerApp(t0,t1,t2,direction):
+def placeholder():
+    pass
+
+def timerApp(timer0,timer1,timer2,direction):
     #timerApp(1,1,1,"down")
 
     global mode, root, sw
@@ -71,30 +152,33 @@ def timerApp(t0,t1,t2,direction):
     sw = StopWatchModule.StopWatch(root)
     sw.pack(side=TOP, expand=YES, fill=BOTH)
     
+    
     if direction is 'up':
         mode = 'stopWatch'
         sw.Start()
-
-        x = threading.Timer(t0, phaseOne, args=(0,))
+        
+        x = threading.Timer(timer0, fetchData, args=(timer1,0,))
         x.start()
 
-        y = threading.Timer(t0+t1, phaseTwo, args=(0,))
+        y = threading.Timer(timer0+timer1, closePorts)
         y.start()   
 
-        z = threading.Timer(t0+t1+t2, phaseThree)
+        z = threading.Timer(timer0+timer1+timer2, placeholder)
         z.start()
+        phaseOne(timer0)
     
     elif direction is 'down':
         mode = 'countDown'
-        sw.countDown(t0)
+        sw.countDown(timer0)
         
-        x = threading.Timer(t0, phaseOne, args=(t1,))
+        x = threading.Timer(timer0, fetchData, args=(timer1,timer2,))
         x.start()
         
-        y = threading.Timer(t0+t1, phaseTwo, args=(t2,))
+        y = threading.Timer(timer0+timer1, closePorts)
         y.start()   
 
-        z = threading.Timer(t0+t1+t2, phaseThree)
+        z = threading.Timer(timer0+timer1+timer2, placeholder)
         z.start()
+        phaseOne(timer0)
 
     root.mainloop()
