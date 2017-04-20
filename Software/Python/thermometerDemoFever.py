94862f13b4eb946f657b33bf27c938ad3362c395
moe93 <m.odeh93@gmail.com>
2017-04-20 14:51:22 -0400

    revert back to normal operation after certain time period

--- Software/Python/thermometerDemoFever.py
+++ Software/Python/thermometerDemoFever.py
@@ -8,6 +8,7 @@ deviceBTAddress = ["00:06:66:8C:D3:2F", "00:06:66:8C:D2:65"]
 BaudRate = 115200
 timeout = 5
 attempts = 3
+simDuration = 20
 
 # create rfObjects/ports and add delay for stability
 rfObject = createPort( deviceName, deviceBTAddress[0], BaudRate, timeout, attempts )
@@ -16,8 +17,16 @@ time.sleep(0.25)
 # Open port 
 rfObject.open()
 
-# Start simulation (hypothermia) then close port
+# Start simulation (fever) then close port
 startSIM_000( rfObject)
+rfObject.close()
+
+# sleep for the duration of the simulation
+time.sleep(simDuration)
+
+# Open port, return to normal operation, then close port
+rfObject.open()
+normalOP(rfObject)
 rfObject.close()
 
 # Release port to avoid permanent connection
