0ccb00b0a24fab2bfdbd1cc531aa5b60ec1059e9
fluviolobo <fluvio.lobo@gmail.com>
2017-11-15 15:41:56 +0000

    updated the recording demo function.

--- Software/Python/stethoscopeDemoRecording.py
+++ Software/Python/stethoscopeDemoRecording.py
@@ -23,8 +23,7 @@ from    stethoscopeProtocol          import *
 print fullStamp() + " Connecting to the Stethoscope"
 deviceName = "SS"
 portNumber = 0
-#deviceBTAddress = "00:06:66:7D:99:D9"
-deviceBTAddress = "00:06:66:8C:D3:F6"
+deviceBTAddress = "00:06:66:8C:9C:2E"
 baudrate = 115200
 attempts = 5
 rfObject = createPort(deviceName,portNumber,deviceBTAddress,baudrate,attempts)
@@ -37,28 +36,7 @@ print fullStamp() + " Triggering Heart Rate Tracking"
 time.sleep(1)
 startRecording(rfObject,attempts)
 
-
-print fullStamp() + " Openning Stethoscope Serial Port"
-time.sleep(1)
-if rfObject.isOpen() == False:
-    rfObject.open()
-
-tracking_start_time = time.time()
-tracking_stop_time = 30
-tracking_current_time = 0
-dataStream = []
-print fullStamp() + " Recording and Tracking Heart Rate for %.03f seconds" %tracking_stop_time
-while tracking_current_time < tracking_stop_time:
-    
-    dataStream.append(["%.02f" %tracking_current_time,
-                       rfObject.readline()[:-1]])
-    
-    tracking_current_time = time.time() - tracking_start_time
-    print fullStamp() + " Current Simulation Time = %.03f" %tracking_current_time
-
-print fullStamp() + " Closing Stethoscope Serial Port"
-time.sleep(1)
-rfObject.close()
+time.sleep(40)
 
 print fullStamp() + " Stopping Device Recording"
 time.sleep(1)
