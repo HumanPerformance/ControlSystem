0ce250cdbbc7a9bdf69bf62473def35c3bbf90f8
fluviolobo <fluvio.lobo@gmail.com>
2017-11-17 20:44:09 +0000

    Began working on the recording function to ensure functionality with the new directory structure.

--- Software/Python/stethoscope/stethoscopeDemoRecording.py
+++ Software/Python/stethoscope/stethoscopeDemoRecording.py
@@ -12,6 +12,12 @@ import  sys
 import  os
 import  serial
 import  time
+
+from    configurationProtocol    import *
+device = "stethoscope"
+homeDir, pythonDir, deviceDir = definePaths(device)
+response = addPaths(pythonDir)
+
 import  stethoscopeDefinitions       as     definitions
 from    os.path                      import expanduser
 from    bluetoothProtocol_teensy32   import *
@@ -22,27 +28,27 @@ from    stethoscopeProtocol          import *
 
 print fullStamp() + " Connecting to the Stethoscope"
 deviceName = "SS"
-portNumber = 0
+portNumber = 1
 deviceBTAddress = "00:06:66:8C:9C:2E"
 baudrate = 115200
 attempts = 5
-rfObject = createPort(deviceName,portNumber,deviceBTAddress,baudrate,attempts)
+rfObject = createBTPort(deviceBTAddress,portNumber)
 
 print fullStamp() + " Enquiring Stethoscope Status"
 time.sleep(1)
-statusEnquiry(rfObject,attempts)
+statusEnquiry(rfObject)
 
 print fullStamp() + " Triggering Heart Rate Tracking"
 time.sleep(1)
-startRecording(rfObject,attempts)
+startRecording(rfObject)
 
-time.sleep(40)
+time.sleep(20)
 
 print fullStamp() + " Stopping Device Recording"
 time.sleep(1)
-stopRecording(rfObject,attempts)
+stopRecording(rfObject)
 
 print fullStamp() + " Releasing Serial Port"
 time.sleep(1)
-portRelease('rfcomm', 0)
+closeBTPort(rfObject)
 
