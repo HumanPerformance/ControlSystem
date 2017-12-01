b9c866a948cfa2061e8ecac51a4bc61e929f740c
fluviolobo <fluvio.lobo@gmail.com>
2017-12-01 15:00:59 +0000

    Built functions for sending/parsing strings instead of bytes to the stethoscope. These functions will be used to change filenames.

---
+++ Software/Python/stethoscope/stethoscopeDemoParsing.py
@@ -0,0 +1,47 @@
+"""
+Stethoscope Demo :: Parsing
+
+Fluvio L Lobo Fenoglietto
+11/30/2017
+"""
+
+# Import
+import  sys
+import  os
+import  serial
+import  time
+
+from    configurationProtocol    import *
+device = "stethoscope"
+homeDir, pythonDir, deviceDir = definePaths(device)
+response = addPaths(pythonDir)
+
+import  stethoscopeDefinitions       as     definitions
+from    os.path                      import expanduser
+from    bluetoothProtocol_teensy32   import *
+from    stethoscopeProtocol          import *
+
+# Operation
+
+
+print fullStamp() + " Connecting to the Stethoscope"
+deviceName = "SS"
+portNumber = 1
+deviceBTAddress = "00:06:66:8C:9C:2E"
+baudrate = 115200
+attempts = 5
+rfObject = createBTPort(deviceBTAddress,portNumber)
+
+print fullStamp() + " Enquiring Stethoscope Status"
+time.sleep(1)
+statusEnquiry(rfObject)
+
+print fullStamp() + " Sending String "
+outString = "HOLA"
+parseString(rfObject,outString)
+
+
+print fullStamp() + " Releasing Serial Port"
+time.sleep(1)
+closeBTPort(rfObject)
+
