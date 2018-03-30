26663e7aa7cd18981833d0f821d1292b2dfdc0a0
moe93 <m.odeh93@gmail.com>
2018-03-30 15:25:09 -0400

    Enquire thermometer

--- Software/Python/get_status.py
+++ Software/Python/get_status.py
@@ -15,14 +15,15 @@
 *
 '''
 
-from    time                        import  sleep, time     # Sleep for stability, time for timing
-from    getpass                     import  getuser         # Get user name on WINDOWS
-from    usbProtocol                 import  createUSBPort   # Connect to USB based device
-from    usbProtocol                 import  createACMPort   # ... ACM variant
-from    bluetoothProtocol_teensy32  import  createBTPort    # Connect to BT based devices
-from    timeStamp                   import  fullStamp       # Time stamps
-##from    smarthandleProtocol         import  triggerDevice   # Not sure why
-import  os, os.path, argparse, platform                     # Various features
+from    time                        import  sleep, time             # Sleep for stability, time for timing
+from    getpass                     import  getuser                 # Get user name on WINDOWS
+from    usbProtocol                 import  createUSBPort           # Connect to USB based device
+from    usbProtocol                 import  createACMPort           # ... ACM variant
+from    bluetoothProtocol_teensy32  import  createBTPort            # Connect to BT based devices
+from    bluetoothProtocol_teensy32  import  closeBTPort             # Disconnect from BT based devices
+from    timeStamp                   import  fullStamp   as  t       # Time stamps
+##from    smarthandleProtocol         import  triggerDevice           # Not sure why
+import  os, os.path, argparse, platform                             # Various features
 
 # ************************************************************************
 # ======================> CONSTRUCT ARGUMENT PARSER <=====================
@@ -37,112 +38,142 @@ args = vars( ap.parse_args() )
 # ************************************************************************
 # =========================> DEFINITIONS & SETUP <========================
 # ************************************************************************
-SOH             = chr(0x01)                                 # Start of Header
-ENQ		= chr(0x05)                                 # Enquiry
-ACK             = chr(0x06)                                 # Positive Acknowledgement
-NAK             = chr(0x15)                                 # Negative Acknowledgement
+SOH             = chr(0x01)                                         # Start of Header
+ENQ		= chr(0x05)                                         # Enquiry
+ACK             = chr(0x06)                                         # Positive Acknowledgement
+NAK             = chr(0x15)                                         # Negative Acknowledgement
 
-if platform.system()=='Linux':                              # Define paths on a LINUX
+if platform.system()=='Linux':                                      # Define paths on a LINUX
 
-    homeDir = "/home/pi"                                    # ...
-    dst = homeDir + "/Desktop/data/"                        # ...
-    fileName = dst + "log.txt"                              # ...
+    homeDir = "/home/pi"                                            # ...
+    dst = homeDir + "/Desktop/data/"                                # ...
+    fileName = dst + "log.txt"                                      # ...
 
 
-elif platform.system()=='Windows':                          # Define paths on a WINDOWS (you are a horible person)
+elif platform.system()=='Windows':                                  # Define paths on a WINDOWS (you are a horible person)
 
-    homeDir = "C:\\Users\\{}\\".format( getuser() )         # ...
-    dst     = homeDir + "Desktop\\data\\"                   # ...
-    fileName= dst + "log.txt"                               # ...
+    homeDir = "C:\\Users\\{}\\".format( getuser() )                 # ...
+    dst     = homeDir + "Desktop\\data\\"                           # ...
+    fileName= dst + "log.txt"                                       # ...
 
 else:
     print( "OS not supported. Aborting" )
     quit()
 
-if ( os.path.exists(dst)==False ):                          # Check if directory exists
-    os.makedirs(dst)                                        # Create folder
-else: pass                                                  # ...
+if ( os.path.exists(dst)==False ):                                  # Check if directory exists
+    os.makedirs(dst)                                                # Create folder
+else: pass                                                          # ...
 
-if( os.path.isfile(fileName)==False ):                      # Check if file exists
-    file(fileName, 'w').close()                             # Create mt file
-else: pass                                                  # ...
+if( os.path.isfile(fileName)==False ):                              # Check if file exists
+    file(fileName, 'w').close()                                     # Create mt file
+else: pass                                                          # ...
 
 # ************************************************************************
 # =============================> SMARTHOLDER <============================
 # ************************************************************************
+__doc__ = '''
+# ************************************************************************ #
+#                                SMARTHOLDER                               #
+# ************************************************************************ #
+'''
+
 port = 0
 baud = 115200
 timeout = 1
 notReady = True
 
-print( "Enquiring SmartHolder..." ) ,                       # [INFO] Status update
+print( __doc__ )                                                    # [INFO] Status update
 
-##SH = createUSBPort( port, baud, timeout )                   # Create connection
-SH = createACMPort( port, baud, timeout )                   # Create connection
-if( SH.is_open ):                                           # Check if port is open
+##SH = createUSBPort( port, baud, timeout )                           # Create connection
+SH = createACMPort( port, baud, timeout )                           # Create connection
+if( SH.is_open ):                                                   # Check if port is open
     pass
 else:
-    SH.open()                                               # else open it if closed
+    SH.open()                                                       # else open it if closed
 
-while( notReady ):                                          # Loop until we receive SOH
-    inData = SH.read( size=1 )                              # ...
-    if( inData == SOH ):                                    # ...
-        print( "Ready!" )                                   # [INFO] Status update
-        break                                               # ...
+while( notReady ):                                                  # Loop until we receive SOH
+    inData = SH.read( size=1 )                                      # ...
+    if( inData == SOH ):                                            # ...
+        print( "{} [INFO] SOH Received".format( t() ) )             # [INFO] Status update
+        break                                                       # ...
 
-sleep( 0.50 )                                               # Sleep for stability
-SH.write( ENQ )                                             # Send ENQUIRY byte
+sleep( 0.50 )                                                       # Sleep for stability
+SH.write( ENQ )                                                     # Send ENQUIRY byte
 
-with open( fileName, 'w' ) as f:                            # Write stuff to file
+with open( fileName, 'w' ) as f:                                    # Write stuff to file
     
-    print( "Starting {} seconds of data collection"         # [INFO] Status update
-           .format( args["time"] ) )                        # [INFO] ...
+    f.write( __doc__ )                                              # Write device header
+    
+    print( "{} Starting {} seconds of data collection"              # [INFO] Status update
+           .format( t(), args["time"] ) )                           # [INFO] ...
 
-    start = time()                                          # Start timer
+    start = time()                                                  # Start timer
     while( time()-start < args["time"] ):
-        inData = "{}".format( SH.readline() )               # Read until timeout is reached
+        inData = "{}".format( SH.readline() )                       # Read until timeout is reached
 
-        if( inData == '' ):                                 # Skip empty lines
-            pass                                            # ...
+        if( inData == '' ):                                         # Skip empty lines
+            pass                                                    # ...
 
-        else:                                               # Else, read incoming data
-            formatted = ( "{} {}"                           # Add time stamp
-                          .format( fullStamp(), inData ) )  # ...
+        else:                                                       # Else, read incoming data
+            formatted = ( "{} {}".format( t(), inData ) )           # Add time stamp
 
-            print( formatted.strip('\n') )                  # [INFO] Status update
-            f.write( formatted )                            # Write to log file
+            print( formatted.strip('\n') )                          # [INFO] Status update
+            f.write( formatted )                                    # Write to log file
             
-SH.close()                                                  # Close port and move on
+SH.close()                                                          # Close port and move on
 
-print( "DONE" )                                             # [INFO] Status update
+print( "{} DONE".format( t() ) )                                    # [INFO] Status update
 
 # ************************************************************************
 # =============================> STETHOSCOPE <============================
 # ************************************************************************
+__doc__ = '''
+# ************************************************************************ #
+#                                STETHOSCOPE                               #
+# ************************************************************************ #
+'''
 
+with open( fileName, 'a' ) as f:                                    # Write stuff to file
+    f.write( __doc__ )                                              # Write device header
 
 # ************************************************************************
 # =============================> THERMOMETER <============================
 # ************************************************************************
-##BT_addr = "00:06:66:8C:D2:65"                               # BT MAC address
-##port = 1                                                    # Port > 0
-##
-##print( "Enquiring Thermometer..." ) ,                       # [INFO] Status update
-##
-##try:
-##    thermo = createBTPort( BT_addr, port )                  # Establish connection
-##
-##    sleep( 0.50 )                                           # Sleep for stability   
-##    thermo.send( ENQ )                                      # Send ENQUIRY byte
-##    inByte = thermo.recv( 1 )                               # Expect 1 byte of data
-##
-##    with open( fileName, 'a' ) as f:                        # Write stuff to file
-##        f.write( "Thermometer...{}\n".format( inByte ) )    # ...
-##
-##except:
-##    print( "[INFO] Error Caught [INFO]..." ) ,              # [INFO] Status update
-##
-##finally:
-##    closeBTPort( thermo )                                   # Close port
-##
-##print( "DONE" )                                             # [INFO] Status update
+__doc__ = '''
+# ************************************************************************ #
+#                                THERMOMETER                               #
+# ************************************************************************ #
+'''
+
+BT_addr = "00:06:66:8C:D2:65"                                       # BT MAC address
+port = 1                                                            # Port > 0
+
+print( __doc__ )                                                    # [INFO] Status update
+
+try:
+    thermo = createBTPort( BT_addr, port )                          # Establish connection
+
+    sleep( 0.50 )                                                   # Sleep for stability   
+    thermo.send( ENQ )                                              # Send ENQUIRY byte
+    inByte = thermo.recv( 1 )                                       # Expect 1 byte of data
+        
+    if( inByte == ACK ):                                            # If ACK
+        formatted = ( "{} Thermometer: ACK\n".format( t() ) )       # ...
+        
+    elif( inByte == NAK ):                                          # If NAK
+        formatted = ( "{} Thermometer: NAK\n".format( t() ) )       # ...
+
+    else:                                                           # Else
+        formatted = ( "{} Thermometer: ERR\n".format( t() ) )       # ...
+
+    with open( fileName, 'a' ) as f:                                # Write stuff to file
+        f.write( __doc__ )                                          # Write device header
+        f.write( formatted )                                        # ...
+
+except:
+    print( "{} [INFO] Error Caught".format( t() ) )                 # [INFO] Status update              
+
+finally:
+    closeBTPort( thermo )                                           # Close port
+
+print( "{} DONE".format( t() ) )                                    # [INFO] Status update
