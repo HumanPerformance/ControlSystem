60099b586fc30a9678df811ecd38aada16795be8
fluviolobo <fluvio.lobo@gmail.com>
2018-04-26 16:17:59 -0400

    specified new variables and ip addresses

--- Software/Python/consys/qtGUIv4.py
+++ Software/Python/consys/qtGUIv4.py
@@ -1036,7 +1036,7 @@ PANEL_COMMAND = {
 "reset"     : "sudo reboot",
 "test"      : "sudo python pd3d/csec/repos/ControlSystem/Software/Python/consys/status.py",
 "consys"    : "sudo python pd3d/csec/repos/ControlSystem/Software/Python/consys/printme.py",
-"scene1"    : "sudo python pd3d/csec/repos/ControlSystem/Software/Python/consys/status.py",
+"scene1"    : "sudo python pd3d/csec/repos/ControlSystem/Software/Python/consys/consys4.3.py",
 "scene2"    : "sudo ls"
 }
 
