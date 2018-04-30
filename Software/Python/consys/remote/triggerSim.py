b8b650cfba2b809f0ad8994ac8ad53077fc5fc9d
fluviolobo <fluvio.lobo@gmail.com>
2018-04-30 18:50:46 -0400

    Changed ip addresses to match the local addresses that will be used for the demo

--- Software/Python/consys/remote/triggerSim.py
+++ Software/Python/consys/remote/triggerSim.py
@@ -15,15 +15,15 @@ import sys
 # -------------------------------------------------------------------------------- #
 # Variables
 # -------------------------------------------------------------------------------- #
-SSH_ADDRESS = (["10.171.190.91",            # panel 001 - 001
-                "10.171.190.201",           # panel 001 - 002
+SSH_ADDRESS = (["192.168.42.10",            # panel 001 - 001
+                "192.168.42.12",            # panel 001 - 002
                 "10.171.190.176",           # panel 002 - 001
                 "10.171.190.90"])           # panel 002 - 002
 SSH_USERNAME = "pi"
 SSH_PASSWORD = "raspberry"
 SSH_DIR = "pd3d/csec/repos/ControlSystem/Software/Python/consys/"
 SSH_COMMAND = (["python " + SSH_DIR + "consys4.3.py",
-                "python " + SSH_DIR + "consys4.3bpc.py"])
+                "DISPLAY=:0 python " + SSH_DIR + "consys4.3bpc.py"])
 ssh = miko.SSHClient()
 ssh.set_missing_host_key_policy(miko.AutoAddPolicy())
 
@@ -33,17 +33,19 @@ ssh_stdin = ssh_stdout = ssh_stderr = None
 # Operation: Communicating with system over ssh
 # -------------------------------------------------------------------------------- #
 try:
-    ssh.connect(SSH_ADDRESS[2], username=SSH_USERNAME, password=SSH_PASSWORD)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(SSH_COMMAND[0])
+    for i in range( 0, 2 ):
+        ssh.connect(SSH_ADDRESS[i], username=SSH_USERNAME, password=SSH_PASSWORD)
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(SSH_COMMAND[i])
     
 except Exception as e:
     sys.stderr.write("SSH connection error: {0}".format(e))
 
+"""
 if ssh_stdout:
     output = ssh_stdout.read()
     print(output)
 
-"""
+
 # extract output flag ------------------------------------------------------------ #
 delimeter = (["\n", " "])
 outList = output.split( delimeter[0] )                                             # split output on the basis of a delimeter
