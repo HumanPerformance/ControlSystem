0b31af3132c0949f81d77ba3299fdc5264baf1e4
fluviolobo <fluvio.lobo@gmail.com>
2018-04-30 10:17:08 -0400

    Updated status check function and polished the file transfer script

---
+++ Software/Python/consys/remote/dataTransfer.py
@@ -0,0 +1,36 @@
+"""
+dataTransfer.py
+
+Script designed to allow remote retrieval of output data
+
+Fluvio L Lobo Fenoglietto 04/30/2018
+"""
+
+# -------------------------------------------------------------------------------- #
+# Import Modules and Libraries
+# -------------------------------------------------------------------------------- #
+import paramiko as miko
+import sys
+
+# -------------------------------------------------------------------------------- #
+# Variables
+# -------------------------------------------------------------------------------- #
+host = '10.171.190.91'
+port = 22
+username = 'pi'
+password = 'raspberry'
+remote_path = '/home/pi/pd3d/csec/repos/ControlSystem/Software/Python/consys/output.zip'
+local_path = 'C:/Users/fluviolobo/Desktop/output.zip'
+
+# -------------------------------------------------------------------------------- #
+# Operation: Communicating with system over ssh
+# -------------------------------------------------------------------------------- #
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname=host, port=port, username=username, password=password)
+sftp = ssh.open_sftp()
+
+sftp.get(remote_path, local_path)
+
+sftp.close()
+ssh.close()
--- Software/Python/consys/remote/statusCheck.py
+++ Software/Python/consys/remote/statusCheck.py
@@ -15,12 +15,14 @@ import sys
 # -------------------------------------------------------------------------------- #
 # Variables
 # -------------------------------------------------------------------------------- #
-SSH_ADDRESS = "10.171.190.91"
+SSH_ADDRESS = (["10.171.190.91",            # panel 001 - 001
+                "10.171.190.201",           # panel 001 - 002
+                "10.171.190.90"])           # panel 002 - 001
 SSH_USERNAME = "pi"
 SSH_PASSWORD = "raspberry"
 SSH_DIR = "pd3d/csec/repos/ControlSystem/Software/Python/consys/"
-SSH_COMMAND = "python " + SSH_DIR + "status4.3.py"
-
+SSH_COMMAND = (["python " + SSH_DIR + "status4.3.py",
+                "python " + SSH_DIR + "status4.3bpc.py"])
 ssh = miko.SSHClient()
 ssh.set_missing_host_key_policy(miko.AutoAddPolicy())
 
@@ -30,8 +32,8 @@ ssh_stdin = ssh_stdout = ssh_stderr = None
 # Operation: Communicating with system over ssh
 # -------------------------------------------------------------------------------- #
 try:
-    ssh.connect(SSH_ADDRESS, username=SSH_USERNAME, password=SSH_PASSWORD)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(SSH_COMMAND)
+    ssh.connect(SSH_ADDRESS[0], username=SSH_USERNAME, password=SSH_PASSWORD)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(SSH_COMMAND[0])
     
 except Exception as e:
     sys.stderr.write("SSH connection error: {0}".format(e))
