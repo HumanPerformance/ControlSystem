0a41d0b00c4005e21a2c4274441bbf621138138e
Fluvio Lobo <fluvio.lobo@gmail.com>
2018-04-30 20:47:36 -0400

    Generated panel-specific scripts to ease demonstration and avoid syntax errors

--- Software/Python/consys/remote/demo/statusCheck_p002001.py
+++ Software/Python/consys/remote/demo/statusCheck_p002001.py
@@ -15,14 +15,11 @@ import sys
 # -------------------------------------------------------------------------------- #
 # Variables
 # -------------------------------------------------------------------------------- #
-SSH_ADDRESS = (["10.171.190.91",            # panel 001 - 001
-                "10.171.190.201",           # panel 001 - 002
-                "10.171.190.90"])           # panel 002 - 001
+SSH_ADDRESS = (["10.171.190.90"])
 SSH_USERNAME = "pi"
 SSH_PASSWORD = "raspberry"
 SSH_DIR = "pd3d/csec/repos/ControlSystem/Software/Python/consys/"
-SSH_COMMAND = (["python " + SSH_DIR + "status4.3.py",
-                "python " + SSH_DIR + "status4.3bpc.py"])
+SSH_COMMAND = (["python " + SSH_DIR + "status4.3bpc.py"])
 ssh = miko.SSHClient()
 ssh.set_missing_host_key_policy(miko.AutoAddPolicy())
 
@@ -32,7 +29,7 @@ ssh_stdin = ssh_stdout = ssh_stderr = None
 # Operation: Communicating with system over ssh
 # -------------------------------------------------------------------------------- #
 try:
-    ssh.connect(SSH_ADDRESS[2], username=SSH_USERNAME, password=SSH_PASSWORD)
+    ssh.connect(SSH_ADDRESS[0], username=SSH_USERNAME, password=SSH_PASSWORD)
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(SSH_COMMAND[0])
     
 except Exception as e:
