8dd5769f31b4906612a96b5ba3a0ff2f92d628d7
fluviolobo <fluvio.lobo@gmail.com>
2018-05-25 16:21:00 +0000

    Completed a self-identification function for the panels. Now we will proceed to pulling the device-associated parameters.

--- Software/Python/consys/configurationProtocol.py
+++ Software/Python/consys/configurationProtocol.py
@@ -83,5 +83,27 @@ def getMAC(interface):
 # Fluvio L Lobo Fenoglietto 05/28/2018
 # ================================================================================= #
 def selfID(id_file_path, device_address):
-    print( fullStamp() + " selfID()" )
+    print( fullStamp() + " selfID()" )                                              # signals execution of the function
+
+    dataFile = open( id_file_path, 'r' )                                            # opens data file with panel information
+    line_num             = 0
+    panel_id_list        = []                                                       # define array variable
+    panel_address_list   = []
+    for line in dataFile:                                                           # for each line in the file ...
+        if len(line) <= 1:                                                          # if nothing in line ... pass
+            pass
+        elif line[0] == "#":                                                        # if line starts with number sign (= comment) ... pass
+            pass
+        else:                                                                       # in any other case ...
+            trim_line = line[:-1]                                                   # trim line by ignoring "end of line" character
+            split_line = trim_line.split(",")                                       # split line with comma delimiter (default)
+            panel_id_list.append( split_line[0] )                                   # append the first element as the number id
+            panel_address_list.append( split_line[1] )                              # append the second element as the MAC address
+            if split_line[1] == device_address:                                     # compare each address to the input panel/device MAC address
+                panel_id        = line_num                                          # if so ... store id number
+                panel_address   = panel_address_list[line_num]                      # if so ... associate address too ...
+
+            line_num = line_num + 1
+
+    return panel_id_list, panel_address_list, panel_id, panel_address
     
--- Software/Python/consys/test.py
+++ Software/Python/consys/test.py
@@ -12,11 +12,14 @@ from    os                          import getcwd, path, makedirs
 # PD3D modules
 from    configurationProtocol       import *
 cons    = "consys"
-homeDir, pythonDir, consDir = definePaths(cons)
+homeDir, pythonDir, consDir, ouputDir, dataDir = definePaths(cons)
 response = addPaths(pythonDir)
 
 from    timeStamp                   import fullStamp
 
 # ------------------ operation
 print( fullStamp() )
-address = getMAC("eth0")
+device_address = getMAC("eth0")
+
+id_file_path = dataDir + "/panels.txt"
+panel_id_list, panel_address_list, panel_id, panel_address = selfID(id_file_path, device_address)
